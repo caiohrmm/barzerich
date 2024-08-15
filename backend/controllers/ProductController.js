194a4647b9e96acc545f19fb33a26f729df4aec8
Caio Henrique <cahenromar@gmail.com>
2024-08-15 10:06:06 -0300

    Implementando endpoints para busca, atualização e remoção de produtos

--- backend/controllers/ProductController.js
+++ backend/controllers/ProductController.js
@@ -1,3 +1,4 @@
+const { Op } = require("sequelize");
 const checkProductExists = require("../helpers/check-product-exists");
 const { Product, Category } = require("../models/associations/associations");
 
@@ -54,6 +55,156 @@ module.exports = class ProductController {
       return res
         .status(500)
         .json({ message: "Erro ao criar produto !", error });
+    }
+  }
+
+  // Listar todos os produtos
+  static async getAllProducts(req, res) {
+    try {
+      const products = await Product.findAll();
+      res.status(200).json(products);
+    } catch (error) {
+      res.status(500).json({ message: "Erro ao listar produtos", error });
+    }
+  }
+
+  // Obter detalhes de um produto
+  static async getProductById(req, res) {
+    const { id } = req.params;
+
+    try {
+      const product = await Product.findByPk(id);
+      if (product) {
+        res.status(200).json(product);
+      } else {
+        res.status(404).json({ message: "Produto não encontrado" });
+      }
+    } catch (error) {
+      res.status(500).json({ message: "Erro ao obter produto", error });
+    }
+  }
+
+  // Atualizar um produto
+  static async updateProduct(req, res) {
+    const { id } = req.params; // Obtém o ID do produto a partir dos parâmetros da URL
+    const { name, salePrice, costPrice, stock, categoryId } = req.body; // Obtém os dados para atualização do corpo da solicitação
+
+    try {
+      // Encontra o produto pelo ID
+      const product = await Product.findByPk(id);
+
+      if (!product) {
+        return res.status(404).json({ message: "Produto não encontrado!" });
+      }
+
+      // Se categoryId foi fornecido, verificar se a categoria existe
+      if (categoryId) {
+        const categoryExists = await Category.findByPk(categoryId);
+
+        if (!categoryExists) {
+          return res.status(404).json({ message: "Categoria não encontrada!" });
+        }
+      }
+
+      // Atualiza os campos do produto se fornecidos no corpo da solicitação
+      if (name) product.nome = name;
+      if (salePrice) product.preco_venda = salePrice;
+      if (costPrice) product.preco_custo = costPrice;
+      if (stock) product.estoque = stock;
+      if (categoryId) product.categoria_id = categoryId;
+
+      // Salva as alterações no banco de dados
+      await product.save();
+
+      res.status(200).json({
+        message: "Produto atualizado com sucesso!",
+        product,
+      });
+    } catch (error) {
+      console.error("Erro ao atualizar produto: " + error);
+      res.status(500).json({ message: "Erro ao atualizar produto!" });
+    }
+  }
+
+  // Excluir um produto
+  static async deleteProduct(req, res) {
+    const { id } = req.params;
+
+    try {
+      const deleted = await Product.destroy({ where: { id } });
+      if (deleted) {
+        res.status(200).json({
+          message: "Produto excluído com sucesso!",
+        });
+      } else {
+        res.status(404).json({ message: "Produto não encontrado" });
+      }
+    } catch (error) {
+      res.status(500).json({ message: "Erro ao excluir produto", error });
+    }
+  }
+
+  static async getProductsByCategory(req, res) {
+    const { categoryId } = req.params; // Obter ID da categoria
+
+    try {
+      // Verifica se a categoria existe
+      const categoryExists = await Category.findByPk(categoryId);
+
+      if (!categoryExists) {
+        return res.status(404).json({ message: "Categoria não encontrada!" });
+      }
+
+      // Encontra todos os produtos da categoria especificada
+      const products = await Product.findAll({
+        where: {
+          categoria_id: categoryId,
+        },
+      });
+
+      res.status(200).json(products);
+    } catch (error) {
+      console.error("Erro ao buscar produtos por categoria: " + error);
+      res
+        .status(500)
+        .json({ message: "Erro ao buscar produtos por categoria!" });
+    }
+  }
+
+  // Buscar produtos por nome
+  static async findProductsByName(req, res) {
+    const { name } = req.query;
+
+    // Verificar se o parâmetro de nome foi fornecido
+    if (!name) {
+      return res
+        .status(422)
+        .json({ message: "O parâmetro 'name' é obrigatório." });
+    }
+
+    try {
+      // Buscar produtos pelo nome
+      const products = await Product.findAll({
+        where: {
+          nome: {
+            [Op.like]: `%${name}%`,
+          },
+        },
+        include: Category, // Opcional: incluir a categoria do produto
+      });
+
+      // Se nenhum produto for encontrado
+      if (products.length === 0) {
+        return res
+          .status(404)
+          .json({ message: "Nenhum produto encontrado com esse nome." });
+      }
+
+      // Retornar os produtos encontrados
+      res.status(200).json(products);
+    } catch (error) {
+      console.error("Erro ao buscar produtos por nome:", error);
+      res.status(500).json({ message: "Erro ao buscar produtos." });
     }
   }
 };
--- backend/routes/ProductRoutes.js
+++ backend/routes/ProductRoutes.js
@@ -3,6 +3,27 @@ const router = require("express").Router();
 const ProductController = require("../controllers/ProductController");
 const checkUser = require("../middlewares/checkUser");
 
+// Adicionar um novo produto
 router.post("/add", checkUser, ProductController.addProduct);
+
+// Listar todos os produtos
+router.get("/", checkUser, ProductController.getAllProducts);
+
+// Obter detalhes de um produto
+router.get("/:id", checkUser, ProductController.getProductById);
+
+// Buscar produtos por nome
+router.get("/findbyname/search", checkUser, ProductController.findProductsByName);
+
+// Atualizar um produto
+router.patch("/:id", checkUser, ProductController.updateProduct);
+
+// Excluir um produto
+router.delete("/:id", checkUser, ProductController.deleteProduct);
+
+// Buscar produtos por categoria
+router.get("/category/:categoryId", checkUser, ProductController.getProductsByCategory);
+
+
 
 module.exports = router;
